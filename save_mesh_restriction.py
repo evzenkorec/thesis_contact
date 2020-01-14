06773e3a03afdee0cb0d76e8238ef77f6816a435
evzenkorec <52708151+evzenkorec@users.noreply.github.com>
2020-01-14 16:59:05 +0100

    Add files via upload

---
+++ save_mesh_restriction.py
@@ -0,0 +1,38 @@
+from dolfin import *
+from multiphenics import *
+import numpy as np
+
+tol = DOLFIN_EPS
+
+penetration = 0.1 # penetration in [mm]
+
+# penetration = np.load('parameters/penetration.npy')
+# penetration = penetration.tolist()
+
+# Number of generated meshes
+num_meshes = np.load('parameters/num_meshes.npy')
+num_meshes = num_meshes.tolist()
+
+for i in range(num_meshes): 
+    # Load mesh
+    mesh = Mesh()
+    with XDMFFile("mesh/mesh_" + str(i) + ".xdmf") as infile:
+        infile.read(mesh)
+    mvc = MeshValueCollection("size_t", mesh, 1)
+    
+    
+    with XDMFFile("mesh/mf_" + str(i) + ".xdmf") as infile:
+        infile.read(mvc, "name_to_read")
+    boundary_markers = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+
+    boundary_markers.array()[boundary_markers.array()>300] = 0
+
+    class OnContactBoundary(SubDomain):
+        def inside(self, x, on_boundary):
+            return on_boundary and x[2] < penetration + tol
+
+    on_contact_boundary = OnContactBoundary()
+    boundary_restriction = MeshRestriction(mesh, on_contact_boundary)
+
+    File("mesh/contact_restriction_boundary_" + str(i) + ".rtc.xml") << boundary_restriction
+    # XDMFFile("mesh/contact_restriction_boundary_" + str(i) + ".rtc.xdmf").write(boundary_restriction)
