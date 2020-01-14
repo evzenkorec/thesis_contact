37c16884b94590c7afe6832d4e70b36c2b786a2f
evzenkorec <52708151+evzenkorec@users.noreply.github.com>
2020-01-14 16:56:09 +0100

    Add files via upload

---
+++ auglag_solver_3D.py
@@ -0,0 +1,200 @@
+from dolfin import *
+from multiphenics import *
+# from ufl import transpose
+import time
+from dolfin import Timer as t
+import numpy as np
+
+def auglag_solver(R,E,nu,penetration,penalty_auglag, mesh, boundary_markers, boundary_restriction):
+
+    class CustomSolver(NewtonSolver):
+        def __init__(self):
+            NewtonSolver.__init__(self, mesh.mpi_comm(),
+                                PETScKrylovSolver(), PETScFactory.instance())
+            self.problem = problem
+
+        def solver_setup(self, A, P, problem, iteration):
+            self.linear_solver().set_operator(A)
+            as_backend_type(A).set_near_nullspace(null_space)
+
+            # Choose conjugate gradient method for the Krylov solver.
+            PETScOptions.set("ksp_type", "gmres")
+            # Output information from the krylov solver process.
+            # PETScOptions.set("ksp_view")
+            #PETScOptions.set("pc_type", "ilu")
+            PETScOptions.set("pc_type", "fieldsplit")
+            PETScOptions.set("pc_fieldsplit_block_size", 3)
+            PETScOptions.set("fieldsplit_pc_type", "hypre")
+            PETScOptions.set("fieldsplit_pc_hypre_type", "boomeramg")
+            PETScOptions.set("fieldsplit_pc_hypre_boomeramg_strong_threshold", 0.7)
+            PETScOptions.set("pc_fieldsplit_0", "0,1,2")
+            # PETScOptions.set("ksp_atol", 1e-11)
+            PETScOptions.set("ksp_rtol", 1e-9)
+            # PETScOptions.set("ksp_rtol", 1e-11)
+            PETScOptions.set("ksp_max_it", 100000)  
+            # PETScOptions.set("ksp_divtol", 1e10)
+            PETScOptions.set("pc_fieldsplit_type", "symmetric_multiplicative")
+            # Set the smoother to be applied tot he MG levels.
+            PETScOptions.set("mg_levels_esteig_ksp_type", "gmres")
+            PETScOptions.set("mg_levels_ksp_type", "chebyshev")
+            PETScOptions.set("mg_levels_pc_type", "jacobi")
+            PETScOptions.set("mg_levels_ksp_chebyshev_esteig_steps", 50)
+            PETScOptions.set("mg_levels_esteig_ksp_max_it", 50)
+            PETScOptions.set("mg_levels_ksp_chebyshev_esteig_random")
+            PETScOptions.set("mg_levels_ksp_max_it", 3)
+            # Set the coarse grid solver.
+            # Solve the coarse grid in parallel across all processes.
+            PETScOptions.set("mg_coarse_ksp_type", "preonly")
+            PETScOptions.set("mg_coarse_pc_type", "lu")
+            PETScOptions.set("mg_coarse_pc_factor_mat_solver_package", "superlu_dist")
+            # Monitor the iterations of the KSP solver to check convergence.
+            # PETScOptions.set("ksp_monitor_true_residual")
+                           
+
+            self.linear_solver().set_from_options()
+            
+        def solve(self):
+            NewtonSolver.solve(self, self.problem, self.problem.block_solution.block_vector())
+            # Keep subfunctions up to date
+            self.problem.block_solution.apply("to subfunctions")
+
+    # Set backend to PETSC
+    parameters["linear_algebra_backend"] = "PETSc"
+
+    def build_nullspace(V, x):
+        """Function to build null space for 3D elasticity"""
+
+        # Create list of vectors for null space
+        nullspace_basis = [x.copy() for i in range(6)]
+
+        # Build translational null space basis
+        V.sub(0).dofmap().set(nullspace_basis[0], 1.0);
+        V.sub(1).dofmap().set(nullspace_basis[1], 1.0);
+        V.sub(2).dofmap().set(nullspace_basis[2], 1.0);
+
+        # Build rotational null space basis
+        V.sub(0).set_x(nullspace_basis[3], -1.0, 1);
+        V.sub(1).set_x(nullspace_basis[3],  1.0, 0);
+        V.sub(0).set_x(nullspace_basis[4],  1.0, 2);
+        V.sub(2).set_x(nullspace_basis[4], -1.0, 0);
+        V.sub(2).set_x(nullspace_basis[5],  1.0, 1);
+        V.sub(1).set_x(nullspace_basis[5], -1.0, 2);
+
+        for x in nullspace_basis:
+            x.apply("insert")
+
+        # Create vector space basis and orthogonalize
+        basis = VectorSpaceBasis(nullspace_basis)
+        basis.orthonormalize()
+
+        return basis                
+    
+    # Form compiler options
+    parameters["form_compiler"]["optimize"]     = True
+    parameters["form_compiler"]["cpp_optimize"] = True
+      
+    boundary_markers.array()[boundary_markers.array()>300] = 0
+        
+    hMESH = CellDiameter(mesh)
+    # meshSIZE = 1/2*(mesh.hmax() + mesh.hmin())
+    meshSIZE = mesh.hmax()
+    
+    V = VectorFunctionSpace(mesh, "Lagrange", 1)
+    Vlm = FunctionSpace(mesh, "Lagrange", 1)
+    W = BlockFunctionSpace([V, Vlm], restrict=[None, boundary_restriction])
+    num_of_dofs = V.dim()
+
+    # TRIAL/TEST FUNCTIONS #
+    dul = BlockTrialFunction(W) # Incremental displacement for Jacobi matrix of iterative "newton_solver"
+    (du, dlm) = block_split(dul)
+    ul = BlockFunction(W)
+    (u, lm) = block_split(ul)
+    vm = BlockTestFunction(W)
+    (v, vlm) = block_split(vm)
+
+    null_space = build_nullspace(V, u.vector())
+
+    # Definition of normal vectors to the elastic body and to the rigid half-space
+    nRIGID = Constant((0.0,0.0,1.0))
+    n = FacetNormal(mesh)
+
+    # Definition of tolerance
+    tol = DOLFIN_EPS # FEniCS tolerance - necessary for "boundary_markers" and the correct definition of the boundaries 
+
+    
+    # Setting the Dirichlet type boundary condition
+    # bc1 = DirichletBC(W.sub(0).sub(2), Constant((-penetration)), boundary_markers, 2) 
+    bc1 = DirichletBC(W.sub(0).sub(2), Constant((-penetration)), boundary_markers, 2) 
+    bP = CompiledSubDomain('sqrt(x[0]*x[0]+x[1]*x[1]) < meshSIZE && near(x[2], R, tol) && on_boundary',meshSIZE = meshSIZE, R=R, tol = tol)
+    bP.mark(boundary_markers, 4)    
+    bc2 = DirichletBC(W.sub(0).sub(0), Constant((0)), boundary_markers, 4)
+    bc3 = DirichletBC(W.sub(0).sub(1), Constant((0)), boundary_markers, 4)
+    bc = BlockDirichletBC([bc1,bc2,bc3])
+    
+    # Definition of contact zones
+    bC1 = CompiledSubDomain('on_boundary && x[2] < penetration + tol', tol=tol, penetration = penetration) # Contact search - contact part of the boundary
+    bC1.mark(boundary_markers, 3)
+
+    # Definition of surface integral subdomains
+    ds = Measure('ds', domain=mesh, subdomain_data=boundary_markers)
+
+
+    # Elasticity parameters
+    mu = E/2/(1+nu) # Elastic parameter mu
+    lmbda = E*nu/(1+nu)/(1-2*nu) # Elastic parameter lambda
+
+    # Spatial coordinates of mesh
+    x = SpatialCoordinate(mesh)
+    
+    
+    # Definition of functions for variational formulation of the contact problem 
+    def epsilon(u): # Definition of deformation tensor
+        return 0.5*(nabla_grad(u) + nabla_grad(u).T)
+    def sigma(u): # Definition of Cauchy stress tensor
+        return lmbda*tr(epsilon(u))*Identity(3) + 2.0*mu*epsilon(u)
+    def gap(u): # Definition of gap function
+        x = SpatialCoordinate(mesh)
+        return x[2]+u[2]
+    def maculay(x): # Definition of Maculay bracket
+        return (x+abs(x))/2
+    def pN(u):
+        return dot(dot(nRIGID,sigma(u)),nRIGID) 
+    def convert_to_integer(a):
+        "Convert to a 32-bit int. Raise exception if this will cause an overflow"
+        if abs(a) <= np.iinfo(np.int32).max:
+            return np.int32(a)
+        else:
+            raise OverflowError("Cannot safely convert to a 32-bit int.")
+  
+    
+    F = [inner(sigma(u), epsilon(v))*dx + 1/(penalty_auglag*E/hMESH)*maculay(-lm-penalty_auglag*E/hMESH*(gap(u)))*(-penalty_auglag*E/hMESH*v[2])*ds(3), 
+     - 1/(penalty_auglag*E/hMESH)*dot(lm,vlm)*ds(3) + 1/(penalty_auglag*E/hMESH)*maculay(-lm-penalty_auglag*E/hMESH*(gap(u)))*(-vlm)*ds(3)]  
+    
+    # Compute Jacobian of F for iterative "newton_solver"
+    J = block_derivative(F, ul, dul)
+    # from IPython import embed;embed()
+    
+    problem = BlockNonlinearProblem(F, ul, bc, J)
+    custom_solver = CustomSolver()
+
+    prm = custom_solver.parameters
+    # prm["absolute_tolerance"] = 1E-9
+    prm["relative_tolerance"] = 1E-9
+    prm["maximum_iterations"] = 100
+    prm["relaxation_parameter"] = 1.0
+    # prm["linear_solver"] = "gmres"
+    # prm["preconditioner"] = "ilu"
+    # prm["krylov_solver"]["absolute_tolerance"] = 1E-9
+    # prm["krylov_solver"]["relative_tolerance"] = 1E-9
+    # prm["krylov_solver"]["maximum_iterations"] = 1000
+
+    custom_solver.parameters.update(prm)
+    
+    with Timer() as t:
+        custom_solver.solve()
+    
+    solution_time = t.elapsed()[0]
+    
+    # u, lm = ul.block_split(deepcopy=False)
+        
+    return solution_time, num_of_dofs
---
+++ command_script_solver.py
@@ -0,0 +1,115 @@
+from penalty_solver_3D import penalty_solver
+from snes_solver_3D import snes_solver
+from nitsche_solver_3D import nitsche_solver
+from nitscheRFP_solver_3D import nitscheRFP_solver
+from auglag_solver_3D import auglag_solver
+from dolfin import *
+from multiphenics import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Text in command window - text output = True, no text = False
+set_log_active(True)
+
+# Geometry parametres
+R = 10 # Radius of the half-sphere in [mm]
+np.save("parameters/R.npy",R)
+
+penetration = 0.1 # penetration in [mm]
+np.save("parameters/penetration.npy",penetration)
+
+
+# Elasticity parameters
+E = 200000. # Young's modulus [MPa]
+np.save("parameters/E.npy",E)
+nu = 0.25 # Poisson ratio [-]
+np.save("parameters/nu.npy",nu)
+
+# penalty parameter
+penalty = 100
+np.save("parameters/penalty.npy",penalty)
+penalty_nitsche = 50
+np.save("parameters/penalty_nitsche.npy",penalty_nitsche)
+penalty_nitsche_RFP = 50
+np.save("parameters/penalty_nitsche_RFP.npy",penalty_nitsche_RFP)
+penalty_auglag = 50
+np.save("parameters/penalty_auglag.npy",penalty_auglag)
+# Parameter of nitscheRFP method
+# theta = 1 - symmetric method
+theta = 1
+np.save("parameters/theta.npy",theta)
+
+# Number of generated meshes
+num_meshes = np.load('parameters/num_meshes.npy')
+num_meshes = num_meshes.tolist()
+
+mesh_size = np.zeros(num_meshes)
+time_penalty = np.zeros(num_meshes)
+time_snes = np.zeros(num_meshes)
+time_nitsche = np.zeros(num_meshes)
+time_nitscheRFP = np.zeros(num_meshes)
+time_auglag = np.zeros(num_meshes)
+
+for i in range(num_meshes): 
+    # Load mesh
+    mesh = Mesh()
+    with XDMFFile("mesh/mesh_" + str(i) + ".xdmf") as infile:
+        infile.read(mesh)
+    mvc = MeshValueCollection("size_t", mesh, 1)
+
+    # num_of_elem = len(mesh.cells())
+    # num_of_dofs = len(V.dofmap().dofs())
+    V = VectorFunctionSpace(mesh, "Lagrange", 1) # Define function space of the problem - piece-wise linear functions
+        
+    with XDMFFile("mesh/mf_" + str(i) + ".xdmf") as infile:
+        infile.read(mvc, "name_to_read")
+    boundary_markers = cpp.mesh.MeshFunctionSizet(mesh, mvc)
+    
+    # semi-circle moves perpendicularly towards the rigid surface a distance of "penetration" in [mm]
+    # penetration = R/50
+      
+    # Solvers list
+    
+    # penalty_solver(R,E,nu,penetration, penalty, mesh, boundary_markers)
+    solution_time, num_of_dofs = penalty_solver(R,E,nu,penetration,penalty, mesh, boundary_markers)    
+    time_penalty[i] = solution_time
+    mesh_size[i] = num_of_dofs
+    # with XDMFFile("u_PEN_" + str(i) + ".xdmf") as xdmf:
+    #     xdmf.write_checkpoint(u, "u", 0.0, XDMFFile.Encoding.HDF5, append=False)
+    
+    # # snes_solver(R,E,nu,penetration, penalty)
+    # num_iter, solution_time, num_of_dofs = snes_solver(R,E,nu,penetration,penalty, mesh, boundary_markers)
+    # iter_snes[i] = num_iter
+    # time_snes[i] = solution_time
+    # # with XDMFFile("u_SNES_" + str(i) + ".xdmf") as xdmf:
+    # #     xdmf.write_checkpoint(u, "u", 0.0, XDMFFile.Encoding.HDF5, append=False)
+
+    solution_time, num_of_dofs = nitsche_solver(R,E,nu,penetration, penalty_nitsche, mesh, boundary_markers)    
+    time_nitsche[i] = solution_time
+    # with XDMFFile("u_NITSCHE_" + str(i) + ".xdmf") as xdmf:
+    #     xdmf.write_checkpoint(u, "u", 0.0, XDMFFile.Encoding.HDF5, append=False)
+
+    solution_time, num_of_dofs = nitscheRFP_solver(R,E,nu,penetration, penalty_nitsche_RFP, theta, mesh, boundary_markers)    
+    time_nitscheRFP[i] = solution_time
+    # with XDMFFile("u_NITSCHE_RFP_" + str(i) + ".xdmf") as xdmf:
+    #     xdmf.write_checkpoint(u, "u", 0.0, XDMFFile.Encoding.HDF5, append=False)
+
+    # boundary_restriction = MeshRestriction(mesh, "mesh/contact_restriction_boundary_" + str(i) + ".rtc.xml")
+    # solution_time, num_of_dofs = auglag_solver(R,E,nu,penetration, penalty_auglag, mesh, boundary_markers, boundary_restriction)
+    # time_auglag[i] = solution_time
+    # # with XDMFFile("u_NITSCHE_RFP_" + str(i) + ".xdmf") as xdmf:
+    # #    xdmf.write_checkpoint(u, "u", 0.0, XDMFFile.Encoding.HDF5, append=False) 
+       
+
+np.save("parameters/mesh_size.npy", mesh_size)
+np.save("parameters/time_penalty.npy", time_penalty)
+np.save("parameters/time_snes.npy", time_snes)
+np.save("parameters/time_nitsche.npy", time_nitsche)
+np.save("parameters/time_nitscheRFP.npy", time_nitscheRFP)
+np.save("parameters/time_auglag.npy", time_auglag)
+
+# Save colored mesh partitions in VTK format if running in parallel
+# for the last mesh
+if MPI.size(mesh.mpi_comm()) > 1:
+    File("mesh/partitions.pvd") << MeshFunction("size_t", mesh, mesh.topology().dim(), \
+                                           MPI.rank(mesh.mpi_comm()))
